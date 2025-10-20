98ca6f27eccf10cae179a717bc5148608bfd04bf
Maily <mai.le@study.hs-duesseldorf.de>
2025-10-20 11:26:14 +0200

    Ordner sortiert

---
+++ Praktikum_Aufgabe_1/bresenham js/bresenham_1.js
@@ -0,0 +1,51 @@
+
+////////////////////////////////////////////////////////////////////////////////
+// bresenham.js
+//
+// Bearbeiten Sie diese Datei für den Praktikumsteil "Bresenham Line".
+//
+// HS Duesseldorf - Fachbereich Medien - Grundlagen d. Computergrafik
+//
+// Studiengang:
+// Gruppe     : K BMI
+// Autor 1    : Mai Ly Le
+// Autor 2    : Emma Damm
+// Autor 3    : Daniela Janjic
+// Autor 4    : Xiaojie Chen
+// Autor 5    : Sara Saeid
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+// drawLine(x0, y0, x1, y1)
+// Diese Funktion soll eine Linie von (x0, y0) nach (x1, y1) zeichnen.
+// Implementieren Sie dazu den Bresenham-Line-Algorithmus für alle Oktanten
+// in dieser Funktion. Einen Punkt zeichnen Sie mit setPixel(x,y).
+////////////////////////////////////////////////////////////////////////////////
+
+
+function drawLine(x0, y0, x1, y1){
+
+
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// example(i)
+// Diese Funktion dient als Codebeispiel.
+// Sie wird beim Laden der Seite aufgerufen und kann entfernt werden.
+////////////////////////////////////////////////////////////////////////////////
+function example(i)
+{
+  let y = i + 2;
+  for (let x = 0; x < 400; x++)
+  {
+    y--;
+    if (y < -i)
+    {
+      y = i;
+    }
+    setPixel(x, Math.abs(y));
+  }
+}
