c97009e223d44cdb78631930665d1616aad43a74
Maily <mai.le@study.hs-duesseldorf.de>
2025-10-23 15:07:39 +0200

    Namens Änderung

--- Praktikum_Aufgabe_1/bresenham js/bresenham_Maily.js
+++ Praktikum_Aufgabe_1/bresenham js/bresenham_Maily.js
@@ -114,41 +114,6 @@ function drawLine(x0, y0, x1, y1){
 
 
 
-
-  /** 
-  // 1. Oktant
-  // x nimmt zu, y nimmt ab
-  let y = y0;
-  let a = dy;       // delta y
-  let b = -(dx);    // -delta x
-
-  // Da im Canvas y nach unten zeigt, invertieren wir a -> siehe Hinweis in Praktikum Aufgabe
-  a = -a;
-
-  // Startwert (Fehlerwert)
-  let Q_init = 2 * a + b;
-  let Q = Q_init;
-  let Q_step = 2 * (a + b);
-  let Q_equal = 2 * a;
-
-  // Von links nach rechts (x0 → x1)
-  for (let x = x0; x <= x1; x++) {
-    setPixel(x, y);  // Pixel setzen
-
-    // Entscheidung
-    if (Q < 0) {
-      Q = Q + Q_equal;
-    } else {
-      Q = Q + Q_step;
-      y--;  // Y-Achse nach unten → Linie "nach oben"
-    }
-  }
-}*/
-
-
-
-
-
 ////////////////////////////////////////////////////////////////////////////////
 // example(i)
 // Diese Funktion dient als Codebeispiel.
